33fe720c664e36437bbcf0a05bfa9aa96262ec63
theKoladeAkande <akandekolade@gmail.com>
2019-09-10 16:19:22 +0100

    Game settings now modularized Instance of spacehip class created to draw spacship to screen

--- fall_of_oar.py
+++ fall_of_oar.py
@@ -1,10 +1,17 @@
 import sys
 
+
 import pygame
+
+
+from space_ship import SpaceShip
+
 
 from settings import Settings
 
+
 __metaclass__ = type 
+
 
 class FallOfOar():
     """ Manages game's properties and assets. """
@@ -17,15 +24,17 @@ class FallOfOar():
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Fall Of Oar")
         #add a  background colour
+        self.spaceship = SpaceShip(self)
 
 
     def run_game(self):
         """ Runs the main loop for the game. """
         while True:
-            #Listens for mouse and keyboard activities
+            #Listens for mouse and keyboard events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.spaceship.blitme()
 
             # Display the drawn screen.
             pygame.display.flip()
